69f19eb19135065eb52fdcd986085334d3e9e208
Mahamud Hridoy <87125242+ZTimu@users.noreply.github.com>
2025-03-23 16:58:12 +0600

    Recreate views directory in src folder

--- server.js
+++ server.js
@@ -1,2 +1,4 @@
 // Server entry point that imports and runs the main application
 const app = require('./src/app');
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('view engine', 'handlebars');
